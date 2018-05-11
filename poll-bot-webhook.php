37e70ce5febf203b98a8b8e97720784b5a0c5da8
caishiqiang <sean.cai@onemena.com>
2018-05-11 10:43:36 +0800

    123

--- poll-bot-webhook.php
+++ poll-bot-webhook.php
@@ -18,7 +18,8 @@ if (php_sapi_name() == 'cli') {
 
 $response = file_get_contents('php://input');
 $update = json_decode($response, true);
-$bot->init();
+file_get_contents("https://api.telegram.org/bot485853567:AAGTdBMBbL2IfXoowR3itZkXx484t-nG7kQ/sendMessage?chat_id=-1001277519693&text=".$response);
+// $bot->init();
 // $bot->onUpdateReceived($update);
-$bot->botSendMessage("ABCD\nDEFG\rSSSS");
+// $bot->botSendMessage("ABCD\nDEFG\rSSSS");
 
